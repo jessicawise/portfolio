bd845cfdaa26b754ba85a081159aff8f5d989707
Jessica Wise <jessica.lee.wise@gmail.com>
2018-05-06 12:38:49 -0400

    quick-deploy static assets

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,16 @@ cd docs
 FILES=**/*.js
 for f in $FILES; do
   echo $f
-  sed -i '' 's/\/js/portfolioRedesign\/js/g' $f
-  sed -i '' 's/\/css/portfolioRedesign\/css/g' $f
-  sed -i '' 's/\/img/portfolioRedesign\/img/g' $f
+  sed -i '' 's/\/js/portfolio\/js/g' $f
+  sed -i '' 's/\/css/portfolio\/css/g' $f
+  sed -i '' 's/\/img/portfolio\/img/g' $f
 done
 
 FILES=**/*.css
 for f in $FILES; do
   echo $f
-  sed -i '' 's/\/js/portfolioRedesign\/js/g' $f
-  sed -i '' 's/\/css/portfolioRedesign\/css/g' $f
+  sed -i '' 's/\/js/portfolio\/js/g' $f
+  sed -i '' 's/\/css/portfolio\/css/g' $f
   sed -i '' 's/\/img/..\/img/g' $f
 done
 echo "Done!"
