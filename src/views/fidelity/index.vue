773cc7fa5933fb9f2e0c0cfce586a4f3af7e0a97
Chris Jennison <cjennison92@gmail.com>
2019-04-12 08:22:15 -0400

    add shell pages

--- src/views/fidelity/index.vue
+++ src/views/fidelity/index.vue
@@ -7,16 +7,18 @@
         </div>
       </div>
       <div class="row"  style='margin-top: 15px;'>
-        <div class="col-md-3 col-md-offset-3">
-          <div class='project-box'>
-
+        <div class="col-md-6 col-md-offset-2">
+          <div class='project'>
+            <router-link to='/works/fidelity/project-1' class='project-box' style="background: url('/img/IMAGE_NAME.png')"></router-link>
+            <span>Project 1</span>
           </div>
 
-          <div class='project-box'>
-
+          <div class='project'>
+            <router-link to='/works/fidelity/project-2' class='project-box' style="background: url('/img/IMAGE_NAME.png')"></router-link>
+            <span>Project 2</span>
           </div>
         </div>
-        <div class="col-md-3" style="text-align:left">
+        <div class="col-md-3" style="text-align:left; padding: 10px;">
           I started at Fidelity in 2017 and worked on a wide variety of projects ranging from internal compliance applications to external client facing financial applications. My roles also varied widely from UX Designer to Design Lead. I did exploration, concepting, wireframing, prototyping and usability testing.
         </div>
       </div>
@@ -41,13 +43,25 @@
       max-width: 300px;
     }
 
+    .project {
+      display: inline-block;
+    }
+
     .project-box {
+      display: block;
       width: 150px;
-      background: grey;
-      border: 1px solid black;
+      border: 3px solid transparent;
       height: 150px;
       margin: 0 10px 0 0;
-      display: inline-block;
+      box-sizing: border-box;
+
+      -webkit-box-shadow: 0px 0px 4px 0px rgba(0,0,0,0.4);
+      -moz-box-shadow: 0px 0px 4px 0px rgba(0,0,0,0.4);
+      box-shadow: 0px 0px 4px 0px rgba(0,0,0,0.4);
+
+      &:hover {
+        border: 3px solid rgba(97, 26, 82, .5);
+      }
     }
   }
 </style>
