fd6c5350fa9c287ac7f3d8709603e1961034f2e5
Jessica Wise <jessica.lee.wise@gmail.com>
2019-04-24 10:20:22 -0400

    Update automated_messaging.vue
    Added case study template

--- src/views/streetsmarts/automated_messaging.vue
+++ src/views/streetsmarts/automated_messaging.vue
@@ -4,6 +4,34 @@
             <div class="row">
                 <div class="col-md-6 col-md-offset-3 align-left">
                     <h2><strong>Automated Messaging</strong></h2>
+                    [Final Design Images]
+
+                    <h3><strong>The Basics</strong></h3>
+
+                    <strong>Role</strong>
+                    Researcher and UX Designer
+                    
+                    <strong>Tools</strong>
+                    Sketch and Invision
+                    
+                    <strong>Time Constraint</strong>
+                    
+
+                    <h3><strong>The Overview</strong></h3>
+
+              
+
+                    <h3><strong>Final Designs</strong></h3>
+                    [Final Images Again]
+
+
+                    <h3><strong>What I Learned</strong></h3>
+
+                   
+                    
+                    <h3><strong>What I Would Change</strong></h3>
+                    
+                  
                 </div>
             </div>
         </div>
