a4f970ed5271afece5bc3f863acf508dec674bc4
Jessica Wise <jessica.lee.wise@gmail.com>
2019-04-24 10:20:47 -0400

    Update tolls.vue
    Added case study template

--- src/views/streetsmarts/tolls.vue
+++ src/views/streetsmarts/tolls.vue
@@ -4,6 +4,32 @@
             <div class="row">
                 <div class="col-md-6 col-md-offset-3 align-left">
                     <h2><strong>Toll Page Redesign</strong></h2>
+                    [Final Design Images]
+
+                    <h3><strong>The Basics</strong></h3>
+
+                    <strong>Role</strong>
+                    Researcher and UX Designer
+                    
+                    <strong>Tools</strong>
+                    Sketch and Invision
+                    
+                    <strong>Time Constraint</strong>
+                    
+
+                    <h3><strong>The Overview</strong></h3>
+
+              
+
+                    <h3><strong>Final Designs</strong></h3>
+                    [Final Images Again]
+
+
+                    <h3><strong>What I Learned</strong></h3>
+
+                   
+                    
+                    <h3><strong>What I Would Change</strong></h3>
                 </div>
             </div>
         </div>
